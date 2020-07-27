81228dcbbc47281cb8f6d6275389a002a764475e
uratomo <rop.ibuki@gmail.com>
2020-07-27 23:07:38 +0900

    リストコンポーネントへのstoreの値引き渡し

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Card from "./card";
+import { connect } from "react-redux";
 
 class CardList extends React.Component {
   render() {
+    const todos = this.props.todos;
+    return todos.map((item, i) => <Card key={i} title={item} />);
   }
 }
 
+const mapStateToProps = (state) => ({ todos: state.todos });
+
+export default connect(mapStateToProps)(CardList);
